a2fe67cd0725df200b3511e2f8461e3a62f1df78
Julius Alvarado <phpninja@mail.com>
2020-05-20 23:54:36 -0700

    C pointers practice, having fun grasping this better 🤓😊🙂

--- ap.c
+++ ap.c
@@ -2,4 +2,17 @@
 // Created by julius on 1/7/2020.
 //
 
+#include "stdio.h"
 #include "ap.h"
+
+
+int morePoints() {
+    int x = 2;
+
+    int *ptX = &x;
+
+    printf("\n----\nx = %d\n", x);
+    printf("ptX = %d\n", *ptX);
+
+    return 0;
+}
